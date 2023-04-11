0054381a76c1fb0c3ccdd47b5493e5fdb44f58fd
fib_nauts <t.si.t.joy7@gmail.com>
2023-04-11 21:48:32 +0900

    add user type

--- src/App.tsx
+++ src/App.tsx
@@ -2,15 +2,23 @@ import { useEffect, useState } from "react";
 import { ListItem } from "./components/ListItem";
 import axios from "axios";
 
+type User = {
+  id: number;
+  name: string;
+  age: number;
+  personalColor: string;
+};
+
+
 export const App = () => {
-  const [users, setUsers] = useState([]);
+  const [users, setUsers] = useState<User[]>([]);
 
   useEffect(() => {
     //    axios.get("https://example.com/users").then((res) => {
     //      setUsers(res.data);
     //    });
 
-    setUsers([
+    setUsers<User[]>([
       { id: 1, name: "nushida", age: 24, personalColor: "blue" },
       { id: 2, name: "sakioka", age: 28, personalColor: "pink" },
       { id: 3, name: "goto", age: 23, personalColor: "green" },

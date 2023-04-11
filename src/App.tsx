aa1313635dd943f716cd6c413bcc7303ae124d80
ooyasik <t.si.t.joy7@gmail.com>
2023-04-11 12:42:06 +0000

    comment out api call

--- src/App.tsx
+++ src/App.tsx
@@ -6,14 +6,20 @@ export const App = () => {
   const [users, setUsers] = useState([]);
 
   useEffect(() => {
-    axios.get("https://example.com/users").then((res) => {
-      setUsers(res.data);
-    })
+    //    axios.get("https://example.com/users").then((res) => {
+    //      setUsers(res.data);
+    //    });
+
+    setUsers([
+      { id: 1, name: "nushida", age: 24, personalColor: "blue" },
+      { id: 2, name: "sakioka", age: 28, personalColor: "pink" },
+      { id: 3, name: "goto", age: 23, personalColor: "green" },
+    ]);
   }, []);
 
   return (
     <div>
-      {users.map(user => (
+      {users.map((user) => (
         <ListItem id={user.id} name={user.nama} age={user.age} />
       ))}
     </div>

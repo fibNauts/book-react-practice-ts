96be33c53cfc20281ba41074996cd23edc6d00a8
fib_nauts <t.si.t.joy7@gmail.com>
2023-04-11 21:06:25 +0900

    set app code

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
-import "./styles.css";
+import { useEffect, useState } from "react";
+import { ListItem } from "./componentes/ListItem";
+import axios from "axios";
 
-export default function App() {
+export const App = () => {
+  const [users, setUsers] = useState([]);
+
+  useEffect(() => {
+    axios.get("https://example.com/users").then((res) => {
+      setUsers(res.data);
+    })
+  }, []);
+
   return (
-    <div className="App">
-      <h1>Hello CodeSandbox</h1>
-      <h2>Start editing to see some magic happen!</h2>
+    <div>
+      {users.map(user => (
+        <ListItem id={user.id} name={user.nama} age={user.age} />
+      ))}
     </div>
   );
-}
+};

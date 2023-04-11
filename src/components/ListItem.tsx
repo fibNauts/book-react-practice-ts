2ba9fc680443e8b092b67a82d757738882be22d8
fib_nauts <t.si.t.joy7@gmail.com>
2023-04-11 21:54:18 +0900

    add prop type of ListItem

--- src/components/ListItem.tsx
+++ src/components/ListItem.tsx
@@ -1,4 +1,10 @@
-export const ListItem = props => {
+type User = {
+  id: number;
+  name: string;
+  age: number;
+};
+
+export const ListItem = (props: User) => {
   const { id, name, age } = props;
   return (
     <p>
